a39a2df4eb8b70be471e7b860fb84504961fc46f
mohamed-harzali <med.harzali2019@gmail.com>
2023-01-05 14:23:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Profile from './component/Profile';
 
-function App() {
+
+class App extends React.Component {
+constructor(props) {
+  super(props);
+
+  this.state = { person:{
+    fullName : "mohamed harzali",
+    profession : "web developer",
+    bio : "19 years old . student",
+    photo : <img className='img' src="./photo.jpg" alt=''></img>},
+    show: false
+  }
+  }
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App-header">
+      {
+        this.state.show?
+          <div className="App-header">
+            <h1>{this.state.person.fullName}</h1>
+            <div>{this.state.person.photo}</div>
+            <h2>{this.state.person.profession}</h2>
+            <p>{this.state.person.bio}</p>
+            <Profile>
+            </Profile>
+          </div>
+        :null
+      }
+      <button type="button" className="btn btn-light" onClick={()=>this.setState({...this.state,show:!this.state.show})}>
+        <h4>Click</h4>
+      </button>
+     </div> 
   );
 }
-
+}
 export default App;
